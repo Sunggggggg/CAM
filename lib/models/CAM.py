0741fadd1269ecc84de5240fcc5a8fc0f06d3b10
sky <sky990319@gmail.com>
2024-05-09 00:21:46 +0900

    f

--- lib/models/CAM.py
+++ lib/models/CAM.py
@@ -5,16 +5,25 @@ import torch.nn as nn
 from lib.models.CTG import CTG
 from lib.models.transformer import Transformer
 
+transpose = lambda x : x.permute(0, 2, 1)
+
 class CAM(nn.Module) :
     def __init__(self, seqlen=16, d_model=2048, num_head=8, spatial_n_layer=3) :
         super().__init__()
         self.spatial_attn = Transformer(depth=spatial_n_layer, embed_dim=seqlen, mlp_hidden_dim=seqlen*2, h=num_head, length=d_model)
         
+
     def forward(self, x) :
         """
         
         """
-        x = x.permute(0, 2, 1)
-        x = self.spatial_attn(x)
+        # 1. Spatial Attention
+        x = transpose(self.spatial_attn(transpose(x)))
         
+        # 2. Context token 
+
+        # 3. Temporal Attention
+        # 4. Global prediction
+        # 5. Local 
+
         return x
--- lib/models/CTG.py
+++ lib/models/CTG.py
@@ -1,25 +1,29 @@
 import torch
 import torch.nn as nn
 
-
 # Context Token Generation Moduel
 class CTG(nn.Module) :
-    def __init__(self, d_model=2048, d_token=256) :
+    def __init__(self, d_model, d_token, abs_flag=False) :
         super().__init__()
+        self.abs_flag = abs_flag
+
         self.proj = nn.Sequential(
             nn.Linear(d_model, d_token),
             nn.LayerNorm(d_token)
         )
-
+    
     def forward(self, x):
         """
-        x : [B, T, C]
+        x : [B, C, T]
         """
-        B, T = x.shape[:2]
+        B, C = x.shape[:2]
 
-        abs_diff = torch.stack([torch.abs(x[:, t+1] - x[:, t]) for t in range(T - 1)], dim=1)  # [B, C]
-        acc_feat = torch.mean(abs_diff, dim=1)
-
+        if self.abs_flag : 
+            diff = torch.stack([torch.abs(x[:, t+1] - x[:, t]) for t in range(C - 1)], dim=1)  # [B, C]
+        else :
+            diff = torch.stack([(x[:, t+1] - x[:, t]) for t in range(C - 1)], dim=1)  # [B, C]
+        
+        acc_feat = torch.mean(diff, dim=1)
         context_token = self.proj(acc_feat)         # [B, T-1, D]
 
 
