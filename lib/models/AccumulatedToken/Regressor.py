978adf3ce8a6757b34f8d617fe686c1002e2ac34
Sunggggggg <sky990319@gmail.com>
2024-05-15 14:10:55 +0900

    f

--- lib/models/AccumulatedToken/Regressor.py
+++ lib/models/AccumulatedToken/Regressor.py
@@ -105,13 +105,13 @@ def regressor_output(pred_pose, pred_shape, pred_cam, seqlen, J_regressor=None) 
 
     pose = rotation_matrix_to_angle_axis(pred_rotmat.reshape(-1, 3, 3)).reshape(-1, 72)
 
-    output = {
+    output = [{
         'theta'  : torch.cat([pred_cam, pose, pred_shape], dim=1).view(-1, seqlen, 85),
         'verts'  : pred_vertices.view(-1, seqlen,6890, 3),
         'kp_2d'  : pred_keypoints_2d.view(-1, seqlen, 49, 2),
         'kp_3d'  : pred_joints.view(-1, seqlen, 49, 3),
         'rotmat' : pred_rotmat.view(-1, seqlen, 24, 3, 3)
-    }
+    }]
 
     return output
 
